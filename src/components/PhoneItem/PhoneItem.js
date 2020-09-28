0ac7fd0e4fdacd851a746619fe743ced5b3bf41f
장재영 <jangjaeyeong@MacBook-Pro-2.local>
2020-09-28 19:42:29 +0900

    이름복구

--- src/components/PhoneItem/PhoneItem.js
+++ src/components/PhoneItem/PhoneItem.js
@@ -13,8 +13,8 @@ const PhoneItem = () => {
     <PhoneWrappers>
       <div className="phone_item">
         <div className="phone_item_left">
-          <div className="phone_item_name">홍길동</div>
-          <div className="phone_item_phone">010-0000-0000</div>
+          <div className="phone_item_name">장재영</div>
+          <div className="phone_item_phone">010-0000</div>
         </div>
         <div className="phone_item_right">
           <button>삭제</button>
